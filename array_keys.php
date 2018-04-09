3c74d4657b1f03bdff6f352f134422ce37b7559a
Oksana <fsoksanna@gmail.com>
2018-04-09 17:10:42 +0300

    adding file to new repository

---
+++ array_keys.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+//---функция array_keys -возвращает все или некоторое подмножество ключей массива
+
+function array_keys_my($arr) 
+{
+	$array = [];                  // объявляем массив, который будем заполнять ключами
+	foreach($arr as $key => $name) 
+		{
+			$array[] = $key;       // проходим по массиву и берем его ключи
+		}
+		return $array; 
+}
+
+$new_arr = [13, 21, 13,];
+
+echo '<pre>';
+var_dump(array_keys_my($new_arr));
+echo '</pre>';
+
+
+?>
