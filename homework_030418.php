7375ec4ce4752ceab1cfdc5ba5374b43429b30bb
Oksana <fsoksanna@gmail.com>
2018-04-11 18:55:07 +0300

    функция Фибоначи переписана через return

--- homework_030418.php
+++ homework_030418.php
@@ -32,7 +32,36 @@ echo '<hr>';
 
 //+ 2 task Вывести определенное количество элементов посл-ти Фибоначи
 
-// вариант 1
+// в стиле ООП--- через---RETURN---
+
+class Fibonachi
+{
+    public $a;
+    public $b;
+    public $c;
+    
+    public function fibo($a, $b, $k)
+    {
+            $res = '';
+            $res .= $a.';'; //ýòîò êîä ïîëíîñòüþ ýêâèâàëåíòåí êîäó $res = $res . 'a';
+            $res .= $b.';';
+            
+            for ($i = 0; $i < $k; $i++) {
+            $c = $a + $b; 
+            $res .= $c.';'; //ýòîò êîä ïîëíîñòüþ ýêâèâàëåíòåí êîäó $res = $res . 'c';
+            $a = $b; 
+            $b = $c;
+        }
+        return $res;
+    }
+}
+
+$obj = new Fibonachi;
+echo $obj->fibo(0, 1, 15);
+echo '<hr>';
+
+
+// вариант simple
 
 $a = 0;
 $b = 1;
@@ -46,51 +75,6 @@ for ($i = 0; $i < 50; $i++) {
     $b = $c;  
 }
 
-// вариант 2 через функцию
-
-function fibonachi($a, $b, $k)
-{
-    echo $a.';';
-    echo $b.';';
-    
-    for ($i = 0; $i < $k; $i++) {
-        $c = $a + $b; 
-        echo $c.';';
-        $a = $b; 
-        $b = $c;
-        $i++;
-    }
-}
-fibonachi(3, 5, 15);
-
-
-// в стиле ООП
-
-class Fibonachi
-{
-
-    public $a;
-    public $b;
-    public $c;
-    
-    public function fibo($a, $b, $k)
-    {
-            echo $a.';';
-            echo $b.';';
-            
-            for ($i = 0; $i < $k; $i++) {
-            $c = $a + $b; 
-            echo $c.';';
-            $a = $b; 
-            $b = $c;
-        }
-    }
-}
-
-$obj = new Fibonachi;
-$obj->fibo(0, 1, 15);
-
-echo '<hr>';
 
 //- 3 task найти в массиве число второе по величине
 
