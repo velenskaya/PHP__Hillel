c034e5984b453e0962fd87a29f47bdaac196bb63
Oksana <fsoksanna@gmail.com>
2018-04-11 20:27:39 +0300

    удалена лишняя проверка, за счет возможности контроля типа данных

--- count.php
+++ count.php
@@ -2,19 +2,22 @@
 
 //функция count - подсчитывает количество элементов массива или что-то в объекте (собственная реализация)
 
-function count_my($arr)
+
+function count_my(array $arr)
 { 
-    if (!is_array($arr)) {
-      return false;
-    } else {
-      $i = 0;  
-      foreach ($arr as $key=>$elem) {
-    	    $i++;
-        } 
-        return $i;
-    }
-    
+    $i = 0;  
+    foreach ($arr as $key=>$elem) {
+        $i++;
+    } 
+     return $i;  
 } 
 
 $array = [18,25,13, 17,90,77,33];
+
 echo count_my($array);
+
+echo '<hr>';
+
+$int =7;
+
+echo count_my($int);
