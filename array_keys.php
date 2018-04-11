e97eb85dc9965a62148f1c47357ec4ec658f95d5
Oksana <fsoksanna@gmail.com>
2018-04-11 19:23:42 +0300

    исправлено условие проверки параметра функции массив или нет

--- array_keys.php
+++ array_keys.php
@@ -2,49 +2,27 @@
 
 //---функция array_keys -возвращает все или некоторое подмножество ключей массива
 
-/*function array_keys_my($arr) 
-{
-	$array = [];              // объявляем массив, который будем заполнять ключами
-	is_array($arr);           // проверка - является ди переданный параметр  массивом           
-	foreach ($arr as $key => $name) 
-		{
-			$array[] = $key;  // проходим по массиву и берем его ключи
-		}
-		return $array; 
-}
-
-$new_arr = [13, 21, 13,];
-
-echo '<pre>';
-var_dump(array_keys_my($new_arr));
-echo '</pre>';
-*/
-
 function array_keys_my($arr) 
 {
-    if (is_array($arr)) {    // проверка - является ди переданный параметр  массивом     
-	$array = [];              // объявляем массив, который будем заполнять ключами
-	               
-	foreach ($arr as $key => $name) 
-		{
-			$array[] = $key;  // проходим по массиву и берем его ключи
-		}
-		return $array;         
-     } else echo "Это не массив";   
+    if (!is_array($arr)) {        // проверка - является ди переданный параметр  массивом   
+        echo "вы ввели число, надо ввести массив";
+        return false;
+    } else  
+      {    
+        $array = [];               // объявляем массив, который будем заполнять ключами
+        foreach ($arr as $key => $name) 
+        {
+           $array[] = $key;      // проходим по массиву и берем его ключи
+        }
+      return $array;         
+      }
 }
-
 $new_arr = [13, 21, 13,];
 $new_arr2 = 56;
-
 echo '<pre>';
 var_dump(array_keys_my($new_arr));
 echo '</pre>';
 
-echo '<pre>';
-var_dump(array_keys_my($new_arr2));
-echo '</pre>';
-
-
-
+echo (array_keys_my($new_arr2));
 
 ?>
