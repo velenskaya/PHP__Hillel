df6af93b329652e37212122a999efaed9c209786
Oksana <fsoksanna@gmail.com>
2018-04-11 19:44:07 +0300

    исправлено замечание на счет проверки

--- count.php
+++ count.php
@@ -1,18 +1,21 @@
 ﻿<?php
 
 //функция count - подсчитывает количество элементов массива или что-то в объекте (собственная реализация)
+
 function count_my($arr)
-{
-    $i = 0;  
-    is_array($arr); 
-	foreach ($arr as $key=>$elem) {
+{ 
+    if (!is_array($arr)) {
+      return false;
+    } else {
+      $i = 0;  
+      foreach ($arr as $key=>$elem) {
     	if ($key >= 0) {
            $i++;
-        } 
-    } return $i;
+         } 
+       } 
+        return $i;
+    }
+    
 } 
-
-$array = [18,25,13, 17,90];
+$array = [18,25,13, 17,90,77,33];
 echo count_my($array);
-
-?>
