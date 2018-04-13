2aed368df902e2ab3a85a9252e3d8276b637f9ee
Oksana <fsoksanna@gmail.com>
2018-04-13 22:10:45 +0300

    добавлена заготовка для task 9

---
+++ task9_strpos.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+//+ 9 task strpos  - Возвращает позицию первого вхождения подстроки (своя реализация функции php)
+
+// int strpos ( string $haystack , mixed $needle [, int $offset = 0 ] )
+
+<?php
+
+function my_strpos($haystack, $needle)
+{
+	
+}
+
+
+
+var_dump(my_strpos($haystack, $needle));
+
+?>
