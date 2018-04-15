8e3b6b5a1fc94f0b1e09bb74a7f3650d6131407f
Oksana <fsoksanna@gmail.com>
2018-04-15 12:39:54 +0300

    решение task9 пока не работает

--- task9_strpos.php
+++ task9_strpos.php
@@ -8,11 +8,39 @@
 
 function my_strpos($haystack, $needle)
 {
+	$i = 0;
+
+	while ( isset($needle[$i]) ) {
 	
+		$i++;
+	}
+
+	$h = 0;
+
+	while ( isset($haystack[$h]) ) { 
+		
+		$bufer ='';
+
+		for ( $j = $h; $j < $h+$i; $j++ ) {
+			
+			$bufer .= $haystack[$j]; 
+		}
+        
+        $h++;
+        
+		if ( $bufer == $needle ) {
+			return $h;
+
+		} return false;
+
+	   
+	}
+
 }
 
 
-
-var_dump(my_strpos($haystack, $needle));
+$haystack ='cabr';
+$needle = 'ab';
+var_dump(my_strpos( $haystack, $needle) );
 
 ?>
