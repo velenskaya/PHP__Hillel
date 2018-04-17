9b7ecfe2fdeb8757ade9a8ac4258ae68265b4a44
Oksana <fsoksanna@gmail.com>
2018-04-17 23:17:34 +0300

    добавление task 7

---
+++ task7_array_diff.php
@@ -0,0 +1,73 @@
+﻿
+<?php
+
+//- 7 task array_diff - Вычислить расхождение массивов (своя реализация функции php)
+
+//----в ООП------------------
+
+class My_array_diff
+{
+    public $arr1; 
+    public $arr2;  
+   
+    public function my_array_diff()
+    {
+        $arr = [];
+        foreach ($this->arr1 as $value1) {
+            $control = false;
+            foreach ($this->arr2 as $value2) {
+                if ($value1 === $value2) {
+                    $control = true;
+                    break;            
+        		}        
+    		}
+		    if ($control === false) {
+		        $arr[] = $value1;
+		    } 
+		}
+   		return $arr;     
+        
+    }
+}
+
+$array1 = [1,2,8,3,77,];
+$array2 = [8,5,1,4,];  
+
+$obj = new My_array_diff();
+$obj->arr1 = $array1;
+$obj->arr2 = $array2;
+
+
+print_r($obj->My_array_diff());
+
+
+
+
+
+
+//-----------------------------
+
+
+$arr1 = [1,2,8, 3];
+$arr2 = [8,5,1,4,];
+
+
+$arr = [];
+foreach ($arr1 as $value1) {
+    $control = false;
+    foreach ($arr2 as $value2) {
+        if ($value1 === $value2) {
+            $control = true;
+            break;            
+        }        
+    }
+    if ($control === false) {
+        $arr[] = $value1;
+    } 
+}
+    
+    //return $arr;
+
+print_r($arr);  
+
+?>
